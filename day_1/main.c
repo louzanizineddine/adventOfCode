5a0f180ac25f89e093217425bcb1e6ab7e46814c
louzanizineddine <louzanizineddine@gmail.com>
2023-12-02 12:46:48 +0100

    found the right answer

--- day_1/main.c
+++ day_1/main.c
@@ -1,36 +1,45 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+int get_first_last_digit(const char *buffer) {
 
+  int first = -1;
+  int last = -1;
 
-int get_first_last_digit(const char *buffer){
-
-    int first = 0;
-    int last = 0;
-
-    for (int i = 0; i < len(buffer); i++)
-    {
-        if (isdigit(buffer[i]) && first == 0){
-            first = isdigit(buffer[i]);
-        }
-
-        
+  for (int i = 0; i < strlen(buffer); i++) {
+    if ((isdigit(buffer[i]) != 0) && (first == -1)) {
+      first = buffer[i] - '0'; 
     }
 
+    if (isdigit(buffer[i]) != 0 && first != -1) {
+      last = buffer[i] - '0';
+    }
+  }
+  
+  printf("first dig = %d last dig = %d \n", first, last);
+
+  // only one digit in the buffer
+  if (last == -1) {
+    return first * 10 + first;
+  }
+
+  return first * 10 + last;
 }
 
-int main ()
-{
+int main() {
 
-    FILE *f = fopen("input.txt", "r");
+  FILE *f = fopen("input.txt", "r");
 
-    char * buffer = NULL;
-    size_t buffer_size = 0;
-    int counter = 1;
-    while (getline(&buffer, &buffer_size, f) != -1)
-    {
-        printf("%s | %d \n", buffer, counter);
-        counter++;
-    }
-    
-    return 0;
+  char *buffer = NULL;
+  size_t buffer_size = 0;
+  int counter = 1;
+  int sum = 0;
+  while (getline(&buffer, &buffer_size, f) != -1) {
+
+    counter = get_first_last_digit(buffer);
+    sum += counter; 
+  }
+  
+  printf("sum = %d\n", sum);
+  return 0;
 }
